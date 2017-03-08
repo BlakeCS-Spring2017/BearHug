21eb6cd8c992c58cf5dc472ffe94195438c2f1ab
ccquinn17 <ccquinn17@TBSLTMBA918.local>
2017-03-08 11:20:24 -0600

    Made a circle with a circle inside it for the dial
    ... also started making a task bar but class ended

--- src/Dial.js
+++ src/Dial.js
@@ -7,6 +7,10 @@ class Dial extends Component {
       <div>
         <h2>Dial</h2>
         <p>Here's the HTML for the dial!</p>
+        <div id="circle">  
+        	<div id="circle2"> </div> 
+        </div>
+        <div id="taskbar"> </div>
         <hr />
       </div>
     );
