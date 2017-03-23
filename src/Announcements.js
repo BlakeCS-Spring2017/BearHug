9030545fa475eee2ecee48bdd8ad009edeb8bda5
sjellis18 <sjellis18@TBSLTMBA982.local>
2017-03-23 08:35:09 -0500

    triangles and new accordion trial

--- src/Announcements.js
+++ src/Announcements.js
@@ -9,14 +9,41 @@ class Announcements extends Component {
           Announcements
         </div>
         <div className="announcement">
+          <span className="announcement-title">
           Anouncement #1
+          </span>
+          <span className="triangle"></span>
         </div>
+
         <div className="announcement">
-          Announcement #2
+          <span className="announcement-title">
+          Anouncement #2
+          </span>
+          <span className="triangle"></span>
         </div>
+
         <div className="announcement">
-          Announcement #3
+          <span className="announcement-title">
+          Anouncement #3
+          </span>
+          <span className="triangle"></span>
         </div>
+
+        <button className="announcement">Announcement #1</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
+        <button className="announcement">Announcement #2</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
+        <button className="announcement">Announcement #3</button>
+            <div class="panel">
+                <p>Lorem ipsum...</p>
+            </div>
+
       </div>
 
     );
