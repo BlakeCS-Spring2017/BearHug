289feadbab819f02043d550d8421ee1f4ee6cbef
sdchute <sdchute18@TBSLTMBA969.local>
2017-03-23 08:30:27 -0500

    Trying again

--- src/Dial.js
+++ src/Dial.js
@@ -7,24 +7,11 @@ class Dial extends Component {
 
     <div>
 
-<<<<<<< Updated upstream
 
-
-
-       		<div className = "ViewContainer2">
-       			<div id="circle2">
-       			</div>
-       		</div>
-
-
-       		<div id="hidingCircle">
-       		</div>
-       		<div id="space2"></div>
-=======
         <svg width="200" height="200">
         <path d="M 50 50 A 25 25, 0, 0 1, 50 100 L 50 90 A 10 10, 0, 0 0, 50 60 Z" fill="green" stroke="black"/>
        	</svg>
->>>>>>> Stashed changes
+
     </div>
     );
   }
