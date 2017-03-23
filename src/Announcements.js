9b86b7a108d8000f2bf584c903dc709438f97d8b
ebroe18 <ebroe18@blakeschool.org>
2017-03-23 12:45:46 -0500

    Flippy Triangle Code

--- src/Announcements.js
+++ src/Announcements.js
@@ -10,34 +10,47 @@ class Announcements extends Component {
         <div className="page-title">
           Announcements
         </div>
-        <div className="announcement">
+        <div className="announcement" class="accordion-body collapse">
           <span className="announcement-title">
+          <button class="btn btn-primary" role="button" href=".triangle" data-toggle="collapse" href=".collapseExample1" aria-expanded="fasle" aria-controls="collapseExample">
           Anouncement #1
-          </span>
-          <span className="triangle"></span>
-        </div>
-
-        <div className="announcement">
-          <span className="announcement-title">
-          <button class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-          Anouncement #2
           </button>
           </span>
-          <span className="triangle"></span>
-          <div class="collapse" id="collapseExample">
+          <span className="triangle collapseExample1"></span>
+          <div class="collapse" className="collapseExample1">
               <div class="description">
                   ...
               </div>
           </div>
         </div>
 
-        <div className="announcement">
+        <div className="announcement" class="accordion-body collapse">
           <span className="announcement-title">
-          Anouncement #3
+          <button class="btn btn-primary" role="button" href=".triangle" data-toggle="collapse" href=".collapseExample2" aria-expanded="fasle" aria-controls="collapseExample">
+          Anouncement #2
+          </button>
           </span>
-          <span className="triangle"></span>
+          <span className="triangle collapseExample2"></span>
+          <div class="collapse" className="collapseExample2">
+              <div class="description">
+                  ...
+              </div>
+          </div>
         </div>
+        <div className="announcement" class="accordion-body collapse">
+          <span className="announcement-title">
+          <button class="btn btn-primary" role="button" href=".triangle" data-toggle="collapse" href=".collapseExample3" aria-expanded="fasle" aria-controls="collapseExample">
+          Anouncement #3
+          </button>
+          </span>
+          <span className="triangle collapseExample3"></span>
+          <div class="collapse" className="collapseExample3">
+              <div class="description">
+                  ...
+              </div>
+          </div>
         </div>
+      </div>
 
     );
   }
