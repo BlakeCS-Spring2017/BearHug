70d00e3ed997e7829b7b6f65ed27d616b5ced54d
llperese17 <llperese17@blakeschool.org>
2017-05-02 13:53:09 -0500

    Read/Unread

--- src/Announcements.js
+++ src/Announcements.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import './Announcements.css';
 
 class Announcements extends Component {
+  constructor(props) {
+      super(props);
+      this.buttonClick = this.buttonClick.bind(this);
+      var storedAnnouncements = localStorage.an
+
+      if(storedAnnouncements === undefined) {
+        storedAnnouncements = [];
+        localStorage.an = JSON.stringify([]);
+      } else {
+        storedAnnouncements = JSON.parse(localStorage.an)
+      }
+      this.state = {readAnnouncements: storedAnnouncements}
+  }
+  buttonClick(e){
+    if (!JSON.parse(localStorage.an).includes(e.currentTarget.id)){
+      console.log(e.currentTarget.id)
+      // e.currentTarget.style.backgroundColor = "#151D21";
+      this.state.readAnnouncements.push(e.currentTarget.id)
+      console.log(this.state.readAnnouncements)
+      localStorage.an= JSON.stringify(this.state.readAnnouncements)
+      console.log(localStorage.an)
+      // this.setState({readAnnouncements: announcements})
+    } else {
+      console.log(localStorage.an)
+    }
+    this.forceUpdate();
+  }
   render() {
       if (this.props.annTotal != null) {
         function objectLength(obj) {
           var annLength = 0;
-          for( var key in obj ) {
-            if( obj.hasOwnProperty(key) ) {
-              ++annLength;
+            for( var key in obj ) {
+              if( obj.hasOwnProperty(key) ) {
+                ++annLength;
+              }
             }
-          }
           return annLength;
         };
+        //if this annoucnemet is in localStorage, then add class to it
         var lenAnn = objectLength(this.props.annTotal);
         var annArray = [];
         var keys = Object.keys(this.props.annTotal);
@@ -22,16 +50,17 @@ class Announcements extends Component {
         var annList = [];
         for (var j = 0; j < lenAnn; ++j) {
           annList.push(
-                <div className="announcement" id={"announcement" + j}>
+                <div className="announcement" id={"announcement" + j} >
                   <span className="announcement-title">
-                    <button role="button" data-toggle="collapse" href={".collapse" + j} aria-expanded="false" aria-controls="collapseExample">
-                      {annArray[j][0]}
+                  <div className={"unread" + (JSON.parse(localStorage.an).includes(annArray[j][0]) ? " readClass" : "")}></div>
+                    <button role="button" id={annArray[j][0]} data-toggle="collapse" href={".collapse" + j} aria-expanded="false" aria-controls="collapseExample" onClick={this.buttonClick}>
+                      {annArray[j][1]}
                       <span className={"triangle collapse" + j}></span>
                     </button>
                   </span>
                   <div className={"collapse collapse" + j}>
                     <div className="description">
-                      {annArray[j][1]}
+                      {annArray[j][2]}
                     </div>
                   </div>
                 </div>
@@ -42,7 +71,7 @@ class Announcements extends Component {
   return (
     <div className="page">
       <div className="Page-Title">
-        Announcements
+        Bulletin
       </div>
 
         
