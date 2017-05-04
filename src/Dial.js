d7e5e06c65e7f006bd9934cd7eeb85fe7de7f1dc
sdchute <sdchute18@TBSLTMBA969.local>
2017-05-04 10:42:54 -0500

    Adding some data for schedules

--- src/Dial.js
+++ src/Dial.js
@@ -17,9 +17,355 @@ class Dial extends Component {
             classTime : 3900,
             currentDisplay : "100",
             currentTimeInSeconds: 100,
-            end: "9:30am",
+            end: "11:05am",
             currentEndMilli: 100,
         };
+
+        this.days = {
+           monday: [
+            {
+              "name": "Advisory",
+              "start": "8:00am",
+              "end": "8:05am",
+              "duration": 5
+            },
+            {
+              "number": 1,
+              "start": "8:10am",
+              "end": "8:55am",
+              "duration": 45
+            },
+            {
+              "number": 2,
+              "start": "9:00am",
+              "end": "9:45am",
+              "duration": 45
+            },
+            {
+              "name": "Tutorial",
+              "start": "9:50am",
+              "end": "10:15am",
+              "duration": 25
+            },
+            {
+              "number": 3,
+              "start": "10:20am",
+              "end": "11:05am",
+              "duration": 45
+            },
+            {
+              "number": 4,
+              "lunch": 2,
+              "start": "11:10am",
+              "end": "11:55am",
+              "duration": 45
+            },
+            {
+              "name": "Lunch",
+              "lunch": 1,
+              "start": "11:10am",
+              "end": "11:40am",
+              "duration": 30
+            },
+            {
+              "number": 4,
+              "lunch": 1,
+              "start": "11:45am",
+              "end": "12:30pm",
+              "duration": 45
+            },
+            {
+              "name": "Lunch",
+              "lunch": 2,
+              "start": "12:00pm",
+              "end": "12:30pm",
+              "duration": 30
+            },
+            {
+              "number": 5,
+              "start": "12:35pm",
+              "end": "1:20pm",
+              "duration": 45
+            },
+            {
+              "number": 6,
+              "start": "1:25pm",
+              "end": "2:10pm",
+              "duration": 45
+            },
+            {
+              "number": 7,
+              "start": "2:15pm",
+              "end": "3:00pm",
+              "duration": 45
+            }
+          ],
+
+          tuesday: [
+            {
+              "number": 1,
+              "start": "8:00am",
+              "end": "9:05am",
+              "duration": 65
+            },
+            {
+              "name": "Assembly",
+              "start": "9:10am",
+              "end": "9:40am",
+              "duration": 30
+            },
+            {
+              "name": "Advisory",
+              "start": "9:45am",
+              "end": "9:50am",
+              "duration": 5
+            },
+            {
+              "number": 4,
+              "start": "9:55am",
+              "end": "11:00am",
+              "duration": 65
+            },
+            {
+              "number": 5,
+              "lunch": 2,
+              "start": "11:05am",
+              "end": "12:10pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 1,
+              "start": "11:05am",
+              "end": "11:30am",
+              "duration": 25
+            },
+            {
+              "number": 5,
+              "lunch": 1,
+              "start": "11:35am",
+              "end": "12:40pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 2,
+              "start": "12:15pm",
+              "end": "12:40pm",
+              "duration": 25
+            },
+            {
+              "number": 7,
+              "start": "12:45pm",
+              "end": "1:50pm",
+              "duration": 65
+            },
+            {
+              "number": 6,
+              "start": "1:55pm",
+              "end": "3:00pm",
+              "duration": 65
+            }
+          ],
+          wednesday: [
+            {
+              "number": 3,
+              "start": "8:30am",
+              "end": "9:35am",
+              "duration": 65
+            },
+            {
+              "name": "Advisory",
+              "start": "9:40am",
+              "end": "10:15am",
+              "duration": 35
+            },
+            {
+              "number": 2,
+              "start": "10:20am",
+              "end": "11:25am",
+              "duration": 65
+            },
+            {
+              "number": 4,
+              "lunch": 2,
+              "start": "11:30am",
+              "end": "12:35pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 1,
+              "start": "11:30am",
+              "end": "12:00pm",
+              "duration": 30
+            },
+            {
+              "number": 4,
+              "lunch": 1,
+              "start": "12:05pm",
+              "end": "1:10pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 2,
+              "start": "12:40pm",
+              "end": "1:10pm",
+              "duration": 30
+            },
+            {
+              "name": "TASC",
+              "start": "1:15pm",
+              "end": "1:50pm",
+              "duration": 35
+            },
+            {
+              "number": 5,
+              "start": "1:55pm",
+              "end": "3:00pm",
+              "duration": 65
+            }
+          ],
+          thursday: [
+            {
+              "number": 2,
+              "start": "8:00am",
+              "end": "9:05am",
+              "duration": 65
+            },
+            {
+              "name": "Assembly",
+              "start": "9:10am",
+              "end": "9:40am",
+              "duration": 30
+            },
+            {
+              "name": "Advisory",
+              "start": "9:45am",
+              "end": "9:50am",
+              "duration": 5
+            },
+            {
+              "number": 1,
+              "start": "9:55am",
+              "end": "11:00am",
+              "duration": 65
+            },
+            {
+              "number": 3,
+              "lunch": 2,
+              "start": "11:05am",
+              "end": "12:10pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 1,
+              "start": "11:05am",
+              "end": "11:30am",
+              "duration": 25
+            },
+            {
+              "number": 3,
+              "lunch": 1,
+              "start": "11:35am",
+              "end": "12:40pm",
+              "duration": 65
+            },
+            {
+              "name": "Lunch",
+              "lunch": 2,
+              "start": "12:15pm",
+              "end": "12:40pm",
+              "duration": 25
+            },
+            {
+              "number": 6,
+              "start": "12:45pm",
+              "end": "1:50pm",
+              "duration": 65
+            },
+            {
+              "number": 7,
+              "start": "1:55pm",
+              "end": "3:00pm",
+              "duration": 65
+            }
+          ],
+          friday: [
+            {
+              "name": "Advisory",
+              "start": "8:30am",
+              "end": "8:35am",
+              "duration": 5
+            },
+            {
+              "number": 2,
+              "start": "8:40am",
+              "end": "9:25am",
+              "duration": 45
+            },
+            {
+              "number": 1,
+              "start": "9:30am",
+              "end": "10:15am",
+              "duration": 45
+            },
+            {
+              "number": 3,
+              "start": "10:20am",
+              "end": "11:05am",
+              "duration": 45
+            },
+            {
+              "number": 4,
+              "lunch": 2,
+              "start": "11:10am",
+              "end": "11:55am",
+              "duration": 45
+            },
+            {
+              "name": "Lunch",
+              "lunch": 1,
+              "start": "11:10am",
+              "end": "11:40am",
+              "duration": 30
+            },
+            {
+              "number": 4,
+              "lunch": 1,
+              "start": "11:45am",
+              "end": "12:30pm",
+              "duration": 45
+            },
+            {
+              "name": "Lunch",
+              "lunch": 2,
+              "start": "12:00pm",
+              "end": "12:30pm",
+              "duration": 30
+            },
+            {
+              "number": 5,
+              "start": "12:35pm",
+              "end": "1:20pm",
+              "duration": 45
+            },
+            {
+              "number": 7,
+              "start": "1:25pm",
+              "end": "2:10pm",
+              "duration": 45
+            },
+            {
+              "number": 6,
+              "start": "2:15pm",
+              "end": "3:00pm",
+              "duration": 45
+            }
+          ]
+        }
     };
 
 
@@ -28,6 +374,8 @@ class Dial extends Component {
         this.calculateRadiansOutside = this.calculateRadiansOutside.bind(this);
         this.intervalID = setInterval(this.setCurrentTime, 33);
         this.numberOfClasses = 7;
+        var now = new Date();
+        this.dayOfWeek = now.getDay(); 
     };
 
     componentWillUnmount() {
@@ -86,9 +434,7 @@ class Dial extends Component {
         this.setState({currentEndMilli : endMilliseconds});
 
         this.subtractTwoTimes();
-        this.setCurrentTimeInMilli();
-
-        
+        this.setCurrentTimeInMilli();  
         
     };
 
@@ -114,14 +460,7 @@ class Dial extends Component {
         var rnMinutes = Math.floor(rnSeconds / 60);
         rnSeconds = rnSeconds % 60;
         var rnHours = Math.floor(rnMinutes / 60);
-        rnMinutes = rnMinutes % 60;
 
-        //  if (rnHours < 10) {
-        //     rnHours = "0" + rnHours
-        // }
-        // if (rnHours > 12) {
-        //     rnHours -= 12
-        // }
         if (rnMinutes < 10) {
             rnMinutes = "0" + rnMinutes
         }
@@ -168,7 +507,7 @@ class Dial extends Component {
         }
 
         this.calculateRadiansOutside()
-       
+        
 
         var point1x = 0;
         var point1y = -1;
