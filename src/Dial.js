2ec69e2b6f15987acc827bed2ac72289543d0bf2
sdchute <sdchute18@TBSLTMBA969.local>
2017-05-08 09:37:48 -0500

    warnings fixed

--- src/Dial.js
+++ src/Dial.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Dial.css';
-import Timer from './Timer';
 
 class Dial extends Component {
     constructor() {
@@ -70,7 +69,7 @@ class Dial extends Component {
         	{"duration":45, "end":"3:00pm", "number":6, "start":"2:15pm"}
     	]; 
     	this.week = [this.monday, this.tuesday, this.wednesday, this.thursday, this.friday];
-    	this.noSchool;
+    	this.noSchool = [];
     };
 
 
@@ -78,7 +77,6 @@ class Dial extends Component {
         this.runDial = this.runDial.bind(this);
         this.calculateRadiansOutside = this.calculateRadiansOutside.bind(this);
         this.intervalID = setInterval(this.runDial, 33);
-        this.state.numberOfClasses = 7;
 
     };
 
@@ -100,16 +98,16 @@ class Dial extends Component {
     returnSecondsOf(timeString) {
     	var ending = timeString;
         var dayHalf = ending.slice(ending.length - 2, ending.length);
-        var ending = ending.slice(0, ending.length - 2);
+        ending = ending.slice(0, ending.length - 2);
         var pos = ending.indexOf(":");
         var endHours = ending.slice(0, pos);
 
         var endMinutes = ending.slice(pos + 1,ending.length);
 
-        endHours = parseInt(endHours);
-        endMinutes = parseInt(endMinutes);
+        endHours = parseInt(endHours, 10);
+        endMinutes = parseInt(endMinutes, 10);
 
-            if (dayHalf === "pm" && endHours != 12 ) {
+            if (dayHalf === "pm" && endHours !== 12 ) {
             endHours += 12
         }
 
@@ -129,8 +127,8 @@ class Dial extends Component {
         var Nminutes = time.getMinutes();
         var Nseconds = time.getSeconds();
 
-        Nminutes = Nminutes + (Nhours * 60);
-        Nseconds = Nseconds + (Nminutes * 60);
+        Nminutes += (Nhours * 60);
+        Nseconds += (Nminutes * 60);
 
     	var currentDay;
     	var dayOfWeek = time.getDay();
@@ -145,11 +143,11 @@ class Dial extends Component {
     	for (var i = 0; i < currentDay.length; i++) {
     		var currentBlock = currentDay[i];
 			var timeString = currentBlock["end"];
-			var blockNow = "Block "+currentBlock["number"];
+			var blockNow = "Block " + currentBlock["number"];
 			var blockNext;
-			var durationTime = currentBlock["duration"]*60;
-			if (i+1 < currentDay.length) {
-				blockNext = "Block "+currentDay[i+1]["number"];
+			var durationTime = currentBlock["duration"] * 60;
+			if (i + 1 < currentDay.length) {
+				blockNext = "Block " + currentDay[i + 1]["number"];
 			}
 			else {
 				blockNext = "nothing"
@@ -163,33 +161,18 @@ class Dial extends Component {
 
 
 
-
-
-
-    	//
-
-
-
         var endSeconds = this.returnSecondsOf(this.state.end);
         var endMilliseconds = endSeconds * 1000
         var CT = this.state.classTime;
         var TL = endSeconds - Nseconds;
-        var updateTimeLeft;
-        if (TL > CT) {
-        	CT = 5*60
-        	TL = TL-CT
-        }
+        
         this.setState({timeLeft : CT - TL, 
-        	numberOfClasses: 
-        	currentDay.length, 
+        	numberOfClasses: currentDay.length, 
         	currentBlock: blockNow, 
         	nextBlock: blockNext, 
         	classTime: durationTime,
         	currentEndMilli : endMilliseconds
         })
-
-
-
 
 
         this.subtractTwoTimes();
@@ -218,16 +201,10 @@ class Dial extends Component {
         var r = this.state.currentEndMilli - this.state.currentTimeInMilli;
         var rnSeconds = Math.floor(r / 1000);
         var rnMinutes = Math.floor(rnSeconds / 60);
-        rnSeconds = rnSeconds % 60;
+        rnSeconds %= 60;
         var rnHours = Math.floor(rnMinutes / 60);
-        rnMinutes = rnMinutes % 60;
+        rnMinutes %= 60;
 
-        //  if (rnHours < 10) {
-        //     rnHours = "0" + rnHours
-        // }
-        // if (rnHours > 12) {
-        //     rnHours -= 12
-        // }
         if (rnMinutes < 10) {
             rnMinutes = "0" + rnMinutes
         }
@@ -235,8 +212,12 @@ class Dial extends Component {
             rnSeconds = "0" + rnSeconds
         }
 
-        var rn = rnMinutes + ":"+ rnSeconds;
-
+        if (rnHours == 0) {
+            var rn = rnMinutes + ":"+ rnSeconds;
+        }
+        else {
+            var rn = rnHours + ":" + rnMinutes + ":"+ rnSeconds;
+        }
 
         this.setState({currentDisplay : rn}); 
     }
@@ -246,6 +227,7 @@ class Dial extends Component {
         var wedgeRadians = this.percentToRadians(1 / this.state.numberOfClasses);
         var currentStartRadians = 0;
         var wedgeArray = [];
+        var textArray = [];
 
         for (var i = 0; i < this.state.numberOfClasses; i++) {
             var ipoint1x = Math.sin(currentStartRadians) * this.thickness * this.gap;
@@ -264,16 +246,30 @@ class Dial extends Component {
                 iorientIn = "0 0";
             }
 
-            var littlePath = "M "+ipoint1x+" "+ipoint1y+" A" +this.thickness * this.gap+ +this.thickness * this.gap+ ", 0, "+iorientOut+" "+ipoint2x+" "+ipoint2y+" L "+ipoint3x+" "+ipoint3y+" A" +this.sthickness+ +this.sthickness+ ", 0, "+iorientIn+" "+ipoint4x+" "+ipoint4y+ " Z";
+        var littlePath = "M "+ipoint1x+" "+ipoint1y+" A" +this.thickness * this.gap+ +this.thickness * this.gap+ ", 0, "+iorientOut+" "+ipoint2x+" "+ipoint2y+" L "+ipoint3x+" "+ipoint3y+" A" +this.sthickness+ +this.sthickness+ ", 0, "+iorientIn+" "+ipoint4x+" "+ipoint4y+ " Z";
             
-            currentStartRadians += wedgeRadians
+        currentStartRadians += wedgeRadians
 
-            wedgeArray.push(
-                <path id={"littlePath" + i} d={littlePath}>
+        wedgeArray.push(
+            <path id={"littlePath" + i} d={littlePath}>
 
-                </path>
-            );
-        }
+            </path>
+        );
+
+        var textRadians = (0.5 * wedgeRadians) + currentStartRadians;
+        var textDistance = (this.thickness + this.sthickness) * 0.5;
+        var textX = Math.sin(textRadians) * textDistance;
+        var textY = -Math.cos(textRadians) * textDistance;
+
+
+
+        textArray.push(
+            <text className="goodFont" x={textX} y={textY} fontSize=".3px" textAnchor="middle" > 
+            
+            </text>
+        );
+        
+    }
 
         this.calculateRadiansOutside()
        
@@ -297,6 +293,7 @@ class Dial extends Component {
 
         var arcPath = "M "+point1x+" "+point1y+" A 1 1, 0, "+orientOut+" "+point2x+" "+point2y+" L "+point3x+" "+point3y+" A" +this.thickness+ +this.thickness+ ", 0, "+orientIn+" "+point4x+" "+point4y+ " Z";
         // above is the path for the outside dial circle
+
 
        
     return (
@@ -346,7 +343,7 @@ class Dial extends Component {
                 7
             </text>
             
-            <text className="time" x="-.23" y=".07" fontSize="0.2px" fill="white">
+            <text className="time" x="0" y=".07" fontSize="0.2px" textAnchor="middle" fill="white">
                 {this.state.currentDisplay}
             </text>
         
@@ -380,19 +377,19 @@ class Dial extends Component {
 
 
 
-var special = {"2017-02-15":[
+// var special = {"2017-02-15":[
 
-{"duration":65,"end":"9:35am","number":3,"start":"8:30am"},
-{"duration":35,"end":"10:15am","name":"Lip Sync Battle","start":"9:40am"},
-{"duration":65,"end":"11:25am","number":2,"start":"10:20am"},
-{"duration":65,"end":"12:35pm","lunch":2,"number":4,"start":"11:30am"},
-{"duration":30,"end":"12:00pm","lunch":1,"name":"First Lunch","start":"11:30am"},
-{"duration":65,"end":"1:10pm","lunch":1,"number":4,"start":"12:05pm"},
-{"duration":30,"end":"1:10pm","lunch":2,"name":"Second Lunch","start":"12:40pm"},
-{"duration":35,"end":"1:50pm","name":"TASC","start":"1:15pm"},
-{"duration":65,"end":"3:00pm","number":5,"start":"1:55pm"}
+// {"duration":65,"end":"9:35am","number":3,"start":"8:30am"},
+// {"duration":35,"end":"10:15am","name":"Lip Sync Battle","start":"9:40am"},
+// {"duration":65,"end":"11:25am","number":2,"start":"10:20am"},
+// {"duration":65,"end":"12:35pm","lunch":2,"number":4,"start":"11:30am"},
+// {"duration":30,"end":"12:00pm","lunch":1,"name":"First Lunch","start":"11:30am"},
+// {"duration":65,"end":"1:10pm","lunch":1,"number":4,"start":"12:05pm"},
+// {"duration":30,"end":"1:10pm","lunch":2,"name":"Second Lunch","start":"12:40pm"},
+// {"duration":35,"end":"1:50pm","name":"TASC","start":"1:15pm"},
+// {"duration":65,"end":"3:00pm","number":5,"start":"1:55pm"}
 
-]}
+// ]}
 
 
 export default Dial;
