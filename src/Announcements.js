3f7845b5f6d4eee35d4e3bfeac80807d257d53a2
llperese17 <llperese17@blakeschool.org>
2017-05-10 19:46:09 -0500

    Line breaks in Announcements

--- src/Announcements.js
+++ src/Announcements.js
@@ -62,6 +62,8 @@ class Announcements extends Component {
         }
         var annList = [];
         for (var j = 0; j < annArray.length; ++j) {
+          var annText = annArray[j][2]
+          // annText = annText.replace(/ _^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS/g, )
           annList.unshift(
                 <div className="announcement" id={"announcement" + j} >
                   <span className="announcement-title">
@@ -73,7 +75,7 @@ class Announcements extends Component {
                   </span>
                   <div className={"collapse collapse" + j}>
                     <div className="description">
-                      {annArray[j][2]}
+                      {annText}
                     </div>
                   </div>
                 </div>
