45b8c0921ccd5fb780336160ea299dc0a8669446
sdchute <sdchute18@TBSLTMBA969.local>
2017-05-27 12:46:22 -0500

    deleting an error in app.js

--- src/App.js
+++ src/App.js
@@ -38,14 +38,6 @@ class App extends Component{
     });
   } 
 
-window.addEventListener("load",function() {
-  // Set a timeout...
-  setTimeout(function(){
-    // Hide the address bar!
-    window.scrollTo(0, 1);
-  }, 0);
-}); 
-
   render(){
     return(
         <Router>
